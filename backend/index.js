e57310c804301b331ef0f53cec23720afce00d85
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2024-03-11 04:51:10 +0500

    added express-validation server side validation

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ const connectToMongo=require('./db');
 
 connectToMongo();
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const User=require('../models/User');
 const router= express.Router();
+const {body,validationResult}=require('express-validator')
+router.post('/',[
+ body('name').isLength({min:3}),
+ body('email').isEmail(),
+ body('password').isLength({min:5}),
+], (req,res)=>{
 
-router.get('/', (req,res)=>{
-    obj={
-        name: 'Farooq',
-        age: 24
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj);
+
+    User.create({
+        name:req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user=>res.json(user))
+   
 })
 
 module.exports = router;
