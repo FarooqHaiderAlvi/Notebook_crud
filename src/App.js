ec0c8779e627c06cb5fd613bbe734f4037590f7c
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2024-04-21 13:41:21 +0500

    added navbar,home,about comp and react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
 import FetchNotes from './Components/FetchNotes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    < >
+    <>
       
-      <FetchNotes/>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/" exact element={<><Navbar/> <Home /></>} />
+          <Route path="/about" exact element={<><Navbar/> <About/></>} />
+          <Route path="/fetchNotes" exact element={<><Navbar/> <FetchNotes/></>} />
+          
+
+        </Routes>
+
+
+      </BrowserRouter>
       
     </>
   );
