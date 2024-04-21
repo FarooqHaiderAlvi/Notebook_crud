d7b5268df9dcfddf32e8656a4eeaf755528cfa9d
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2024-04-21 15:08:56 +0500

    use useLocation hook to style navbar active

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,15 +3,12 @@ import { useContext } from "react";
 import noteContext from "../context/noteContext";
 export default function Home(){
 
-const {person,setPerson}=useContext(noteContext)
 
- const changeName=()=>{
-    setPerson({name:'imran'})
- }
+
+ 
 return(
     <>
-    I am Home. {person.name}
-    <button onClick={changeName}>change name </button>
+    I am Home.    
     </>
 ); 
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 export default function Navbar() {
 
+    const location = useLocation()
+    useEffect(() => {
+        console.log(location)
+    }, [location])
 
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">My NoteBook</Link>
+                    <NavLink className="navbar-brand" to="/">My NoteBook</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                <NavLink className={`nav-link  ${location.pathname === '/' ? "active" : ""}`} active aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/about">About</Link>
+                                <NavLink className={`nav-link  ${location.pathname === '/about' ? "active" : ""}`} to="/about">About</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/fetchNotes">Fetch Notes</Link>
+                                <NavLink className={`nav-link  ${location.pathname === '/fetchNotes' ? "active" : ""}`} to="/fetchNotes">Fetch Notes</NavLink>
                             </li>
 
 
